69eee5383aa4852cf1f5b5814683de264f714b35
divyasolanki24 <divyatailor18@gmail.com>
2025-03-10 00:29:53 +0530

    Add shop_management_app/lib/routes/app_routes.dart 1741546792377

---
+++ shop_management_app/lib/routes/app_routes.dart
@@ -0,0 +1,12 @@
+abstract class Routes {
+  static const INITIAL = '/';
+  static const LOGIN = '/login';
+  static const DASHBOARD = '/dashboard';
+  
+  // Shop routes
+  static const SHOPS = '/shops';
+  static const SHOP_CREATE = '/shops/create';
+  static const SHOP_EDIT = '/shops/:id/edit';
+  static const SHOP_DETAIL = '/shops/:id';
+  static const SHOP_PRODUCTS = '/shops/:id/products';
+}
